0898823555b738ed381ad4297ee36e9e5e0358aa
ojasiiitd <ojassaxena03@gmail.com>
2019-06-19 22:12:20 +0530

    Error handling

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,14 @@ function createInputs()
 {
     // getting number of boxes to ceate
     var elements = parseInt(document.getElementById("elements").value);
+    // error handler
+    if(!(elements))
+        errorMsg;
+    
+    var test_case_number = parseInt(document.getElementById("testcases").value);
+    // error handler    
+    if(!(test_case_number))
+        errorMsg();
 
     for(var i=0 ; i<elements ; i++)
     {
@@ -25,7 +33,6 @@ function createInputs()
 // to remove all boxes in case of error
 var reset_btn = document.getElementById("reset");
 
-
 reset_btn.addEventListener("click" , removeBoxes);
 function removeBoxes()
 {
@@ -43,6 +50,12 @@ function generate()
     // final testcases string declaration
     var testcases = "";
 
+    // total number of testcases needed
+    var test_case_number = parseInt(document.getElementById("testcases").value);
+
+    // for errors, reset
+    var reset_btn = document.getElementById("reset");
+
     // getting info from all created boxes
     var newBoxes = document.querySelectorAll("#added-boxes");
     for(var i=0 ; i<newBoxes.length ; i++)
@@ -52,13 +65,10 @@ function generate()
         var code = newBoxes[i].value.toLowerCase().split(" ");
         console.log(code);
 
-        // total number of testcases needed
-        var test_case_number = parseInt(document.getElementById("testcases").value);
-
         // getting all elements by value
         var type = code[0],
-            lowerBound = parseInt(code[1]) || 0,
-            upperBound = parseInt(code[2])+1 || 0,
+            lowerBound = parseInt(code[1]) || -1,
+            upperBound = parseInt(code[2])+1 || -1,
             times = parseInt(code[3]) || 0;
 
         // debug
@@ -66,6 +76,7 @@ function generate()
 
         // to paste testcases here
         var answer = document.getElementById("answer");
+        answer.value = "";
         
         // test_case_number times generation
         for(var test=0 ; test<test_case_number ; test++)
@@ -74,14 +85,32 @@ function generate()
             var add;
             if(type === "int")
             {
+                // error handler
+                if(lowerBound == -1 || upperBound == -1 || times != 0)
+                {
+                    errorMsg();
+                    return;    
+                }
                 add = String(getRandomNumber(lowerBound , upperBound));
             }
             else if(type === "char")
             {
+                // error handler
+                if(lowerBound != -1 || upperBound != -1 || times != 0)
+                {
+                    errorMsg();
+                    return;
+                }
                 add = String(getRandomChar());
             }
             else if(type === "sp-int")
             {
+                // error handler
+                if(lowerBound == -1 || upperBound == -1 || times == 0)
+                {
+                    errorMsg();
+                    return;
+                }
                 // element for space separated integers
                 var intelt = "";
                 for(var j=0 ; j<times ; j++)
@@ -90,6 +119,12 @@ function generate()
             }
             else if(type === "string")
             {
+                // error handler
+                if(lowerBound == -1 || upperBound == -1 || times != 0)
+                {
+                    errorMsg();
+                    return;
+                }
                 // for individual characters
                 var strelt = "",
                     len = getRandomNumber(lowerBound , upperBound);
@@ -97,11 +132,28 @@ function generate()
                     strelt += getRandomChar();
                 add = (strelt + "\n");
             }
+            else
+            {
+                errorMsg();
+                reset_btn.click();
+                return;
+            }
             testcases += (add + "\n");
         }
     }
     // debug
     console.log(testcases);
+    
+    // show on the page
+    answer.value += (testcases + "\n");
+}
+
+// handling errors
+function errorMsg()
+{
+     // Error
+     console.log("Error");
+     alert("There seems to be an error in your input, please check Format tab for more info.")
 }
 
 // for a random integer in [min , max]
