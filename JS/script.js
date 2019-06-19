5585a8276ad0f540c1be34ded13c2c3a9ef554e5
ojasiiitd <ojassaxena03@gmail.com>
2019-06-19 15:38:36 +0530

    Trying a new approach

--- JS/script.js
+++ JS/script.js
@@ -1,62 +1,11 @@
-var submit = document.querySelector("button");
-submit.addEventListener("click" , generate);
-var copybtn = document.querySelector("click" , copytc);
+var input_boxes = document.getElementById("input_boxes");
 
-function generate()
+input_boxes.addEventListener("click" , createInputs());
+function createInputs()
 {
-    var int = document.querySelector("#int");
-    var spint = document.querySelector("#sp-int");
-    var tc = parseInt(document.querySelector("#tc").value);
+    var test_case_number = document.getElementById("testcases").value;
+    var elements = document.getElementById("elements").value;
 
-    if(int.checked)
-    {
-        var from = parseInt(document.querySelector("#intfrom").value),
-            to = parseInt(document.querySelector("#intto").value);
-    }
-    else
-    {
-        from = null;
-        to = null;
-    }
-    if(spint.checked)
-    {
-        var spfrom = parseInt(document.querySelector("#spintfrom").value),
-            spto = parseInt(document.querySelector("#spintto").value),
-            vals = parseInt(document.querySelector("#count-val").value);
-    }
-    console.log(from , to , spfrom , spto);
-
-    var output = "";
-    for(var i=0 ; i<tc ; i++)
-    {
-        output += randomselect(from , to) + "<br>";
-        for(var j=0 ; j<vals ; j++)
-            output += randomselect(spfrom , spto) + " ";
-        output += "<br>"; 
-    }
-    if(output)
-    {
-        var fill = document.querySelector(".fill");
-        fill.innerHTML = output;
-    }
-    else
-    {
-        var error = "<strong is-danger is-size-3>Cannot generate, check again.</strong>";
-        fill.innerHTML = error;
-    }
-    
-}
-
-function copytc()
-{
-    var testcases = document.getElementsByTagName("pre").value;
-    console.log(testcases);
-    // testcases.select();
-    // testcases.execCommand("copy");
-}
-
-function randomselect(min , max)
-{
-    var rand = Math.floor(Math.random() * (max - min + 1) + min);
-    return rand;
+    console.log(test_case_number);
+    console.log(elements);
 }
